db009ae19ab16cd623d1b9a8fac86dd7ec871f70
Julius Dejon <juliusdejon@gmail.com>
2024-04-02 02:47:37 -0400

    remove mock data

--- screens/Home.js
+++ screens/Home.js
@@ -6,72 +6,15 @@ const avatar = require("../assets/memoji.png");
 
 function HomeScreen(props) {
   const { user } = props;
-  const [myListings, setMyListings] = useState([
-    {
-      address: "1-99 Stockton St San Francisco 94108",
-      driveTrain: "Front-Wheel Drive",
-      latitude: 37.7865731,
-      licensePlate: "BLH123",
-      longitude: -122.4066146,
-      msrp: "47499",
-      name: "Ford Escape Plug-In Hybrid",
-      ownerEmail: "owner@email.com",
-      photo:
-        "https://assets.zappyride.com/img/vehicles/chromestyle/423530/style-set-1280/2022FOS160001_1280_01.png",
-      price: "321",
-      seating: "5",
-    },
-    {
-      address: "1-99 Stockton St San Francisco 94108",
-      driveTrain: "Front-Wheel Drive",
-      latitude: 37.7865731,
-      licensePlate: "BLH123",
-      longitude: -122.4066146,
-      msrp: "47499",
-      name: "Ford Escape Plug-In Hybrid",
-      ownerEmail: "owner@email.com",
-      photo:
-        "https://assets.zappyride.com/img/vehicles/chromestyle/423530/style-set-1280/2022FOS160001_1280_01.png",
-      price: "321",
-      seating: "5",
-    },
-    {
-      address: "1-99 Stockton St San Francisco 94108",
-      driveTrain: "Front-Wheel Drive",
-      latitude: 37.7865731,
-      licensePlate: "BLH123",
-      longitude: -122.4066146,
-      msrp: "47499",
-      name: "Ford Escape Plug-In Hybrid",
-      ownerEmail: "owner@email.com",
-      photo:
-        "https://assets.zappyride.com/img/vehicles/chromestyle/423530/style-set-1280/2022FOS160001_1280_01.png",
-      price: "321",
-      seating: "5",
-    },
-    {
-      address: "1-99 Stockton St San Francisco 94108",
-      driveTrain: "Front-Wheel Drive",
-      latitude: 37.7865731,
-      licensePlate: "BLH123",
-      longitude: -122.4066146,
-      msrp: "47499",
-      name: "Ford Escape Plug-In Hybrid",
-      ownerEmail: "owner@email.com",
-      photo:
-        "https://assets.zappyride.com/img/vehicles/chromestyle/423530/style-set-1280/2022FOS160001_1280_01.png",
-      price: "321",
-      seating: "5",
-    },
-  ]);
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const results = await getRentalListingsByEmail(user);
-  //     console.log(results);
-  //     setMyListings(results);
-  //   }
-  //   fetchData();
-  // }, []);
+  const [myListings, setMyListings] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const results = await getRentalListingsByEmail(user);
+      console.log(results);
+      setMyListings(results);
+    }
+    fetchData();
+  }, [user]);
 
   return (
     <View
@@ -100,6 +43,9 @@ function HomeScreen(props) {
       <View style={{ paddingTop: 20 }}>
         <Text style={{ fontSize: 24, fontWeight: "800" }}>My Listings</Text>
         <ScrollView horizontal={true} contentContainerStyle={{}}>
+          {myListings.length === 0 && (
+            <Text style={{ paddingVertical: 80 }}>Empty Listings</Text>
+          )}
           {myListings.map((listing, index) => (
             <Card
               key={`${listing.name}-${index}`}
